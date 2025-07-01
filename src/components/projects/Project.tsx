c8fa049b69f0a2252426bdcb032c72229002bda0
Bhaveshanjana <bhaveshanjana58@gmail.com>
2025-07-01 13:50:38 +0530

    add project

--- src/components/projects/Project.tsx
+++ src/components/projects/Project.tsx
@@ -1,14 +1,19 @@
-import React from "react";
-import ProjectCard from "./ProjectCard";
+"use client";
 
-  type project = {
-    title: string;
-    image: string;
-    github?: string;
-    link?: string;
-    tech: string[];
-  };
+import React, { useState } from "react";
+import ProjectCard from "./ProjectCard";
+import { ShinyButton } from "../ui/button";
+
+type project = {
+  title: string;
+  image: string;
+  github?: string;
+  link?: string;
+  tech: string[];
+};
 const Project = () => {
+  const [showAll, setShowAll] = useState(false);
+
   const data: project[] = [
     {
       github: "https://github.com/Bhaveshanjana/TravoBot",
@@ -44,14 +49,51 @@ const Project = () => {
       link: "https://wipebg.vercel.app/",
       tech: ["React", "ExpressJs", "NodeJs", "TailwindCSS", "RapidApi"],
     },
+    {
+      title: "Artistly",
+      image: "/projects/Artistly.png",
+      github: "https://github.com/Bhaveshanjana/Artistly",
+      link: "https://artistly-rho-three.vercel.app/",
+      tech: ["NextJs", "TailwindCSS", "Shadcn", "Motion"],
+    },
+    {
+      title: "Blogify",
+      image: "/projects/Blogify.png",
+      github: "https://github.com/Bhaveshanjana/Blogify",
+      link: "https://blogify-one-alpha.vercel.app/",
+      tech: [
+        "NextJs",
+        "TailwindCSS",
+        "Shadcn",
+        "Motion",
+        "Prisma",
+        "PostgreSQL",
+      ],
+    },
+    {
+      title: "Eclypse",
+      image: "/projects/Eclypse.png",
+      github: "https://github.com/Bhaveshanjana/Web-Assignment03",
+      link: "https://eclypse-sigma.vercel.app/",
+      tech: ["ReactJs", "TailwindCSS", "NodeJS", "ExpressJs"],
+    },
   ];
+
+  const displayedProjects = showAll ? data : data.slice(0, 4);
   return (
     <div>
       <h1 className="dark:text-gray-300 mt-9 md:text-xl mx-4">Projects</h1>
       <div className="md:grid lg:grid-cols-2 md:space-x-2.5 mx-4 flex flex-col gap-1">
-        {data.map((projectItem) => (
+        {displayedProjects.map((projectItem) => (
           <ProjectCard key={projectItem.title} project={projectItem} />
         ))}
+      </div>
+      <div className="text-center">
+        {data.length > 4 && (
+          <ShinyButton onClick={() => setShowAll(!showAll)} className="">
+            {showAll ? "Show less" : "Show all"}
+          </ShinyButton>
+        )}
       </div>
     </div>
   );
