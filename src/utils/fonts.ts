727da933c3574ea7b8a0a32dcd3382ee2a683579
Bhaveshanjana <bhaveshanjana58@gmail.com>
2025-08-21 10:16:55 +0530

    add more fonts

--- src/utils/fonts.ts
+++ src/utils/fonts.ts
@@ -1,4 +1,4 @@
-import { Bricolage_Grotesque, Manrope } from "next/font/google";
+import { Bricolage_Grotesque, Manrope, Open_Sans, Quicksand, Urbanist } from "next/font/google";
 
 const bricolage_grotesque_init = Bricolage_Grotesque({
   subsets: ["latin"],
@@ -10,5 +10,26 @@ const manrope_init = Manrope({
   display: "swap",
 });
 
+const quick_init = Quicksand({
+  subsets: ["latin"],
+  display: "swap",
+  weight: "300",
+});
+
+const open_sans = Open_Sans({
+  subsets: ["latin"],
+  display: "swap",
+  weight: "300"
+});
+
+const urbanist_init = Urbanist({
+  subsets: ["latin"],
+  display: "swap",
+  weight: "300"
+})
+
 export const bricolage_grotesque = bricolage_grotesque_init.className;
 export const manrope = manrope_init.className;
+export const quick = quick_init.className;
+export const sans = open_sans.className;
+export const urban = urbanist_init.className;
