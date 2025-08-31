04ac319c6f2b67db6961b179f21430b95118a749
Bhaveshanjana <bhaveshanjana58@gmail.com>
2025-08-31 11:35:50 +0530

    add reposwitch project

--- src/components/projects/Project.tsx
+++ src/components/projects/Project.tsx
@@ -18,29 +18,29 @@ type project = {
 const Project = () => {
   const data: project[] = [
     {
+      github: "https://github.com/Bhaveshanjana/BUZZBOT",
+      title: "BuzzBot",
+      image: "/projects/BuzzBot.png",
+      category: "main",
+      description:
+        "A MCP server utility where users can tweet directly from CLI without opening Twitter.",
+    },
+    {
+      title: "RepoSwitch",
+      image: "/projects/RepoSwitch.png",
+      category: "main",
+      github: "https://github.com/Bhaveshanjana/git-switch",
+      link: "https://git-switch.vercel.app/",
+      description:
+        "A one-click tool that lets users instantly switch their GitHub repository visibility between public and private.",
+    },
+    {
       github: "https://github.com/Bhaveshanjana/TravoBot",
       link: "https://travo-bot.vercel.app/",
       title: "TravoBot",
       image: "/projects/TravoBot.png",
       category: "main",
       description: "An AI-based travel itinerary generator powered by Gemini.",
-    },
-    {
-      title: "DaignoBot",
-      image: "/projects/DaignoBot.png",
-      github: "https://github.com/Bhaveshanjana/DiagnoBot",
-      link: "https://diagno-bot.vercel.app/",
-      category: "main",
-      description:
-        "An AI-assisted health checker where users input symptoms to get predicted conditions.",
-    },
-    {
-      github: "https://github.com/Bhaveshanjana/BUZZBOT",
-      title: "BuzzBot",
-      image: "/projects/BuzzBot.png",
-      category: "main",
-      description:
-        "A MCP server utility where users can tweet directly from VS Code without opening Twitter.",
     },
     {
       title: "WipeBg",
@@ -61,12 +61,13 @@ const Project = () => {
         "Dynamic Blog Platform Allowing Users to Write, Publish, and Read Blogs built with next and typescript",
     },
     {
-      title: "SSM",
-      image: "/projects/Ssm.png",
-      category: "saas",
-      link: "https://ssmin.vercel.app/",
+      title: "DaignoBot",
+      image: "/projects/DaignoBot.png",
+      github: "https://github.com/Bhaveshanjana/DiagnoBot",
+      link: "https://diagno-bot.vercel.app/",
+      category: "main",
       description:
-        "Landing page for an Finance company 'SSM'. built with next and motion",
+        "An AI-assisted health checker where users input symptoms to get predicted conditions.",
     },
     {
       title: "OpenSphere",
@@ -74,8 +75,14 @@ const Project = () => {
       category: "saas",
       link: "https://opensphere.vercel.app/",
       github: "https://github.com/Bhaveshanjana/OpenSphere",
-      description:
-        "An mini Saas project. built with next and motion",
+      description: "Built a landing page, with next and motion.",
+    },
+    {
+      title: "SSM",
+      image: "/projects/Ssm.png",
+      category: "saas",
+      link: "https://ssmin.vercel.app/",
+      description: "Built a landing page for an client, with next and motion.",
     },
     {
       title: "Eclypse",
@@ -83,7 +90,7 @@ const Project = () => {
       category: "saas",
       link: "https://eclypse-sigma.vercel.app/",
       github: "https://github.com/Bhaveshanjana/Web-Assignment03",
-      description: "Landing Page from an assignment",
+      description: "Landing page from an assignment.",
     },
   ];
 
@@ -102,25 +109,21 @@ const Project = () => {
           <ProjectCard key={project.title} project={project} />
         ))}
       </div>
-      {/* Saas projects heading */}
       <h3 className="dark:text-gray-200 text-sm md:text-[16px] mt-5 mb-2 bg-gradient-to-r from-[#d3d7da53] dark:bg-gradient-to-r dark:from-[#0C1C21] border-l-2">
-        <span className={`ml-2 ${sans}`}>Some Saas Projects</span>
+        <span className={`ml-2 ${sans}`}>Some landing page</span>
       </h3>
-      {/* Saas projects */}
+      {/* Landing page's */}
       <div className="grid md:grid-cols-2 gap-2">
         {saasProjects.map((sas, index) => (
           <div
             key={index}
-            className="relative border border-gray-300 hover:border-gray-400 dark:border-dashed dark:hover:border-[#525256] dark:hover:bg-[#131314] dark:border-[#27272a] rounded-xs hover:shadow-xl transition-all duration-300  overflow-hidden shadow-lg group "
+            className="relative border border-gray-300 hover:border-gray-400 dark:border-dashed dark:hover:border-[#525256] dark:hover:bg-[#131314] dark:border-[#27272a] rounded-xs hover:shadow-xl transition-all duration-300  overflow-hidden shadow-[0_1px_1px_rgba(0,0,0,0.05),0_4px_6px_rgba(34,42,53,0.04),0_24px_68px_rgba(47,48,55,0.05),0_2px_3px_rgba(0,0,0,0.04)] group "
           >
+            {/* Corner effect */}
             <div>
               <span className="absolute top-0 left-0 w-2 h-2 border-t border-l border-gray-600 dark:border-gray-100  opacity-0 group-hover:opacity-100 transition duration-500"></span>
               <span className="absolute top-0 right-0 w-2 h-2 border-t border-r border-gray-600 dark:border-gray-100  opacity-0 group-hover:opacity-100 transition duration-500"></span>
-
-              {/* Bottom-left corner */}
               <span className="absolute bottom-0 left-0 w-2 h-2 border-b border-l border-gray-600 dark:border-gray-100  opacity-0 group-hover:opacity-100 transition duration-500"></span>
-
-              {/* Bottom-right corner */}
               <span className="absolute bottom-0 right-0 w-2 h-2 border-b border-r border-gray-600 dark:border-gray-100  opacity-0 group-hover:opacity-100 transition duration-500"></span>
             </div>
             <div className="relative h-[200px]">
@@ -137,7 +140,9 @@ const Project = () => {
             <div>
               {/* Project title & Link's */}
               <div className="flex justify-between items-center mb-1 mx-2.5">
-                <h3 className={`dark:text-gray-100 text-gray-700 text-sm ${urban}`}>
+                <h3
+                  className={`dark:text-gray-100 text-black text-sm ${urban}`}
+                >
                   {sas.title}
                 </h3>
                 <div className="flex gap-2">
