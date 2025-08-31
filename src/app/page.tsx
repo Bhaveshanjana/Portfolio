ee85dc23215e97c7300c1819872a9a7ab3c2958d
Bhaveshanjana <bhaveshanjana58@gmail.com>
2025-08-31 11:39:57 +0530

    update motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
+"use client";
 import Contact from "@/components/Contact";
 import Hero from "@/components/Hero";
 import Projects from "@/components/projects/Project";
 import TechStack from "@/components/TechStack";
 import React from "react";
 import SectionEffect from "@/components/utils/textEffect";
+import { motion, Variants } from "framer-motion";
 
+const getBlurVariants = (delay: number): Variants => ({
+  hidden: { opacity: 0, filter: "blur(15px)" },
+  visible: {
+    opacity: 1,
+    filter: "blur(0px)",
+    transition: {
+      duration: 0.5,
+      ease: "easeOut",
+      delay,
+    },
+  },
+});
 const page = () => {
   return (
     <div>
@@ -17,12 +31,17 @@ const page = () => {
         <SectionEffect delay={0.2}>
           <TechStack />
         </SectionEffect>
-
-        <SectionEffect delay={0.3}>
+        
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          viewport={{ once: true, amount: 0.3 }}
+          variants={getBlurVariants(0.3)}
+        >
           <Projects />
-        </SectionEffect>
+        </motion.div>
 
-        <SectionEffect delay={0.4}>
+        <SectionEffect delay={0.2}>
           <Contact />
         </SectionEffect>
       </div>
